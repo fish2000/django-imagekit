e63277cd4d5fd00b3668f2478dbc988155b39c73
FI$H 2000 <fish2000@gmail.com>
2011-10-06 08:05:50 -0400

    Minor changes to Atkinson cython processor cdef.

--- imagekit/ext/processors.pyx
+++ imagekit/ext/processors.pyx
@@ -9,9 +9,13 @@ cdef extern from "processors.h":
 cdef class Atkinsonify:
     
     cdef readonly double threshold
+    cdef unsigned char threshold_matrix[255]
     
     def __cinit__(self, float threshold=128.0):
         self.threshold = threshold
+        
+        for i in xrange(255):
+            self.threshold_matrix[i] = int(i/self.threshold)
     
     def process(self, pilimage, format, obj):
         in_array = misc.fromimage(pilimage, flatten=True)
@@ -24,22 +28,16 @@ cdef class Atkinsonify:
     
     def atkinson(self, numpy.ndarray[numpy.uint8_t, ndim=2, mode="c"] image_i not None):
         
-        cdef int w, h, i, off, err
-        cdef unsigned char threshold_matrix[255]
+        cdef int w, h, i, err
         cdef unsigned char old, new
-        
-        for i in xrange(255):
-            threshold_matrix[i] = int(i/self.threshold)
         
         w = image_i.shape[0]
         h = image_i.shape[1]
         
         for y in xrange(h):
             for x in xrange(w):
-                #off = (y * w) + x
-                
                 old = image_i[x, y]
-                new = threshold_matrix[old]
+                new = self.threshold_matrix[old]
                 err = (old - new) >> 3
                 
                 image_i[x, y] = adderror(image_i[x, y], err)
