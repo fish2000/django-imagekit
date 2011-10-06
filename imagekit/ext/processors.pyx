c4e6805d1ed4f30f24ce6cefd81010f23acb346a
FI$H 2000 <fish2000@gmail.com>
2011-10-06 13:03:00 -0400

    Added StentifordModel skeleton and a few function cdefs in Cython to imagekit/ext/processors.pyx.

--- imagekit/ext/processors.pyx
+++ imagekit/ext/processors.pyx
@@ -39,16 +39,19 @@ cdef class Atkinsonify:
         for i in xrange(255):
             threshold_matrix[i] = int(i/self.threshold)
     
-    def process(self, pilimage, format, obj):
+    @cython.boundscheck(False)
+    def process(self not None, pilimage not None, format not None, obj not None):
         in_array = misc.fromimage(pilimage, flatten=True).astype(numpy.uint8)
         self.atkinson(in_array)
         pilout = misc.toimage(in_array)
         return pilout, format
     
     @cython.boundscheck(False)
+    @cython.wraparound(False)
     cdef inline void atkinson(self, numpy.ndarray[numpy.uint8_t, ndim=2, mode="c"] image_i):
         
-        cdef int x, y, w, h, i, err
+        cdef int x, y, w, h, i
+        cdef int err
         cdef unsigned char old, new
         
         w = image_i.shape[0]
@@ -67,5 +70,78 @@ cdef class Atkinsonify:
                         image_i[nxy[0], nxy[1]] = (image_i[nxy[0], nxy[1]] + err)
                     except IndexError:
                         pass
+
+
+cdef class StentifordModel:
     
+    cdef inline void distance(self,
+        numpy.ndarray[numpy.uint32_t, ndim=2, mode="c"] matrix_a,
+        numpy.ndarray[numpy.uint32_t, ndim=2, mode="c"] matrix_b,
+        numpy.ndarray[numpy.uint32_t, ndim=2, mode="c"] matrix_o):
+        
+        matrix_o = numpy.sum(numpy.abs(matrix_a - matrix_b))
+    
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    cdef inline void hsv_to_rgb(self, float h, float s=1, float v=1, float r=0.0, float g=0.0, float b=0.0):
+        cdef int I
+        cdef double H, f, p, q, t
+        
+        if s == 0.0:
+            r = v
+            g = v
+            b = v
+            return
+        
+        H = h * 6.0
+        I = <int>H
+        f = H - I
+        
+        p = v * (1.0 - s)
+        q = v * (1.0 - s * f)
+        t = v * (1.0 - s * (1.0 - f))
+        
+        if I == 0 or I == 6:
+            #return v, t, p
+            r = v
+            g = t
+            b = p
+            return
+        
+        if I == 1:
+            #return q, v, p
+            r = q
+            g = v
+            b = p
+            return
+        
+        if I == 2:
+            #return p, v, t
+            r = p
+            g = v
+            b = t
+            return
+        
+        if I == 3:
+            #return p, q, v
+            r = p
+            g = q
+            b = v
+            return
+        
+        if I == 4:
+            #return t, p, v
+            r = t
+            g = p
+            b = v
+            return
+        
+        if I == 5: 
+            #return v, p, q
+            r = v
+            g = p
+            b = q
+            return
+    
+
                 
