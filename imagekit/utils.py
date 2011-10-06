1505099b238e8fd78d9fe4068c7b22233ac89505
FI$H 2000 <fish2000@gmail.com>
2011-10-06 18:59:20 -0400

    REBASE #16 -- aha yeah but my ICC profiling ImageProcessors can stay, those are cool dogggg

--- imagekit/utils.py
+++ imagekit/utils.py
@@ -14,3 +14,28 @@ def img_to_fobj(img, format, **kwargs):
     img.save(tmp, format, **kwargs)
     tmp.seek(0)
     return tmp
+<<<<<<< HEAD
+=======
+
+
+def pil_for_lut(lut):
+    """
+    make a PIL image from an RGB LUT of format:
+    [
+        (r,g,b),
+        (r,g,b),
+        ...
+    ]
+    where r, g, b are 0-255. 
+    
+    """
+    from imagekit.lib import Image, ImageCms
+    
+    outlut = Image.new('RGB', (1, len(lut)+2))
+    outpix = outlut.load()
+    
+    for p in range(len(lut)):
+        outpix[0,p] = lut[p]
+    
+    return outlut
+>>>>>>> 319667b... Refactored lcms-based profile/proof transform processors and added base classes for spec/processor chains that yield something other than an image (e.g. a numpy matrix for example).
